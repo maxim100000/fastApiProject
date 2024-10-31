6d1a95293467eadf754284c0c865907206f4be58
max <max@maxc.max>
2024-10-31 14:07:28 +0700

    initial

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,6 +1,5 @@
 from sqlmodel import create_engine
 from .settings import Settings
-import os
 
 settings = Settings()
 
--- app/utils/settings.py
+++ app/utils/settings.py
@@ -4,4 +4,4 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     url: str
 
-    model_config = SettingsConfigDict(env_file="app/.env")
+    model_config = SettingsConfigDict(env_file=".env")
