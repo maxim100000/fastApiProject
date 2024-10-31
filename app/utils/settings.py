0ae586fa139ccefb47ecad3733bbfab62c2be2ba
maxim100000 <105789888+maxim100000@users.noreply.github.com>
2024-10-31 15:43:48 +0700

    Update settings.py

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    url: str
+# class Settings(BaseSettings):
+#     url: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+#     model_config = SettingsConfigDict(env_file=".env")
